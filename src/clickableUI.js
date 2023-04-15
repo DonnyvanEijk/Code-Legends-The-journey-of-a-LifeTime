ad43e101606f54d809adf3a1f7bcff19a5164932
Marc van Elswijk <9018248@student.zadkine.nl>
2023-04-15 18:40:23 +0200

    audio gefixt

--- src/clickableUI.js
+++ src/clickableUI.js
@@ -22,11 +22,8 @@ let startingmenu = document.getElementById("startingmenu")
 
 function changeMenu() {
 	startingmenu.style.opacity = "0"
+	audioPage1.play();
 }
-
-let audioPage1 = new Audio();
-audioPage1.src = "../music/silly_tutorial_music.mp3";
-audioPage1.play();
 
 //variabelen :((()))
 
@@ -96,7 +93,6 @@ questitems.addEventListener("click", function () {
 })
 
 
-var pattern1 = ['c', 'l', 'o', 'c', 'k'];
 let baronquest = document.getElementById("quest1")
 var current1 = 0;
 var container1 = document.getElementById(".gamecontainer");
@@ -104,104 +100,7 @@ let claimed1;
 let claimed2;
 let claimed3;
 let quest1done = false;
-
 let completedbaron = false;
-var keyHandler1 = function (event) {
-	//zorg ervoor dat de pattern zichtbaar wordt
-
-	if (pattern1.indexOf(event.key) < 0 || event.key !== pattern1[current1]) {
-		current1 = 0;
-		return;
-	}
-
-
-	current1++;
-
-
-	if (pattern1.length === current1) {
-		if(quest1done == true) {
-			baronquest.innerHTML = "Je hebt deze quest al voltooid!"
-
-		}
-		
-
-
-		if (openable = true && quest1done == false) {
-			baronquest.innerHTML = "Find the original code for the professor"
-			claimed1 = "claimable"
-			console.log("yes?")
-			quest1done = true;
-		}
-
-
-
-	}
-
-};
-
-document.addEventListener('keydown', keyHandler1, false);
-
-
-
-
-
-
-
-
-
-
-//rewardcodes
-
-var pattern2 = ['5', 'e', '1', 'e', 'o'];
-let aardappeltest = false
-var current2 = 0;
-let rewards;
-
-var container2 = document.getElementById(".gamecontainer");
-var keyHandler2 = function (event) {
-	//zorg ervoor dat de pattern zichtbaar wordt
-
-	if (pattern2.indexOf(event.key) < 0 || event.key !== pattern2[current2]) {
-		current2 = 0;
-		return;
-	}
-
-
-	current2++;
-
-
-	if (pattern2.length === current2) {
-		
-		if (claimed1 == "claimable" && quest1done  == true) {
-			questrewardtext.innerHTML = "Click here for more information!"
-			questrewardtext.style.display = "block"
-			claimed1 = "unclaimable";
-			if (rewards == false) {
-				rewards = true;
-			}
-			quest1done = true
-
-
-
-		}
-	
-
-
-
-
-
-
-
-
-	}
-
-};
-
-
-document.addEventListener('keydown', keyHandler2, false);
-
-
-
 
 //krijg rewards van de quest met toggles
 questrewardtext.addEventListener("click", function () {
@@ -242,7 +141,8 @@ questrewardtext.addEventListener("click", function () {
 
 
 //audiofiles variables
-
+let audioPage1 = new Audio();
+audioPage1.src = "../music/silly_tutorial_music.mp3";
 
 
 
