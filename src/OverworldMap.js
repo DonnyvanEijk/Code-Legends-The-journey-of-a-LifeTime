9af02ba428e5d38f03452f7b944471a5a1c172a6
Marc van Elswijk <9018248@student.zadkine.nl>
2023-04-16 10:14:46 +0200

    labrooms en machinebuttons talk

--- src/OverworldMap.js
+++ src/OverworldMap.js
@@ -251,7 +251,7 @@ window.OverworldMaps = {
   },
   CodeRoom: {
    
-    lowerSrc: "./images/maps/CoderoomLower.png",
+    lowerSrc: "./images/maps/labroom_portal_off.png",
     upperSrc: "./images/maps/CoderoomUpper.png",
     gameObjects: {
       hero: new Person({
@@ -269,6 +269,54 @@ window.OverworldMaps = {
               { type: "textMessage", text: "It is a Computer" },
               { type: "textMessage", text: "It looks complicated" },
               
+             
+            ]
+          }
+        ]
+      }),
+
+      COMPUTER2: new Person({
+        x: utils.withGrid(8),
+        y: utils.withGrid(19),
+        src: "./images/characters/people/Interactable_obkect.png",
+        talking: [
+          {
+            events: [
+              { type: "textMessage", text: "It is a Computer" },
+              { type: "textMessage", text: "It looks complicated" },
+              
+             
+            ]
+          }
+        ]
+      }),
+
+      HINT1: new Person({
+        x: utils.withGrid(9),
+        y: utils.withGrid(19),
+        src: "./images/characters/people/Interactable_obkect.png",
+        talking: [
+          {
+            events: [
+              { type: "textMessage", text: "Look at the buttons" },
+              { type: "textMessage", text: "purple, green, blue, green and yellow" },
+              { type: "textMessage", text: "What can this mean?" },
+             
+            ]
+          }
+        ]
+      }),
+
+      HINT1_2: new Person({
+        x: utils.withGrid(10),
+        y: utils.withGrid(19),
+        src: "./images/characters/people/Interactable_obkect.png",
+        talking: [
+          {
+            events: [
+              { type: "textMessage", text: "Look at the buttons" },
+              { type: "textMessage", text: "purple, green, blue, green and yellow" },
+              { type: "textMessage", text: "What can this mean?" },
              
             ]
           }
@@ -337,7 +385,7 @@ window.OverworldMaps = {
 
 
    [utils.asGridCoord(4, 8)]: true,
-   [utils.asGridCoord(4, 7)]: true,
+   [utils.asGridCoord(6, 7)]: true,
    [utils.asGridCoord(6, 8)]: true,
    [utils.asGridCoord(6, 7)]: true,
    [utils.asGridCoord(5, 6)]: true,
